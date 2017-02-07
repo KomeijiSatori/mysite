70aec6626b2c03ea9f2c433f253d886507e73856
Satori <KomeijiSatori07@gmail.com>
2017-02-07 19:24:32 +0800

    second commit

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Blog
+
+
+admin.site.register(User)
+admin.site.register(Blog)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length = 16, null = False)
+    password = models.CharField(max_length = 40, null = False)
+    email = models.EmailField(null = False)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 50, null = True)
+    author = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
+    publish_time = models.DateTimeField(auto_now_add = True, auto_now = False)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title + " by " + self.author + " at " + self.publish_time
