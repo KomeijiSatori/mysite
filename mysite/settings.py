43069fcc898330e8c684d36715105b118125bc1d
Satori <KomeijiSatori07@gmail.com>
2017-02-26 23:57:27 +0800

    local deploy success

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,9 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static_for_ind", "my_static"),
 # ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = '/'
 
