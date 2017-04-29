91a75482b5f2999febb353252e54b12ed35e7250
Satori <KomeijiSatori07@gmail.com>
2017-04-29 11:45:57 +0800

    fix static files issue

---
+++ static/blogs/render-content.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    marked.setOptions({
+           gfm: true,
+           tables: true,
+           breaks: true,
+           pedantic: false,
+           sanitize: true,
+           smartLists: true,
+           smartypants: false
+       });
+    $(".marked_truncate").each(function() {
+        var content = $(this).text();
+        var rendered_html = marked(content);
+        var truncated_html = jQuery.truncate(rendered_html, {length: 120, words: true});
+        $(this).html(truncated_html);
+    });
+    $(".blog_content").each(function() {
+        var content = $(this).text();
+        var rendered_html = marked(content);
+        $(this).html(rendered_html);
+    });
+});
