c5278a5fdfa18b47b6b54abde009782ae0332b72
Satori <KomeijiSatori07@gmail.com>
2017-09-20 16:40:35 +0800

    fix error when background images fewer than required

--- mysite/templatetags/mysite_tags.py
+++ mysite/templatetags/mysite_tags.py
@@ -11,19 +11,21 @@ register = template.Library()
 def load_background():
     image_show_count = 6  # cannot be larger than the amount of images
     curdir = os.path.join(settings.MEDIA_ROOT, "background")
-    items = os.listdir(curdir)
-    selected = [items[i] for i in random.sample(range(len(items)), image_show_count)]
-    urls = ["/media/background/{}".format(x) for x in selected]
-
-    return mark_safe("""
-        <section class="hidden-xs">
-            <ul class="cb-slideshow">
-                <li><span style="background-image: url('{}')"></span></li>
-                <li><span style="background-image: url('{}')"></span></li>
-                <li><span style="background-image: url('{}')"></span></li>
-                <li><span style="background-image: url('{}')"></span></li>
-                <li><span style="background-image: url('{}')"></span></li>
-                <li><span style="background-image: url('{}')"></span></li>
-            </ul>
-        </section>
-    """.format(*urls))
+    if os.path.exists(curdir):
+        items = os.listdir(curdir)
+        if len(items) >= image_show_count:
+            selected = [items[i] for i in random.sample(range(len(items)), image_show_count)]
+            urls = [settings.MEDIA_URL + "background/{}".format(x) for x in selected]
+            return mark_safe("""
+                <section class="hidden-xs">
+                    <ul class="cb-slideshow">
+                        <li><span style="background-image: url('{}')"></span></li>
+                        <li><span style="background-image: url('{}')"></span></li>
+                        <li><span style="background-image: url('{}')"></span></li>
+                        <li><span style="background-image: url('{}')"></span></li>
+                        <li><span style="background-image: url('{}')"></span></li>
+                        <li><span style="background-image: url('{}')"></span></li>
+                    </ul>
+                </section>
+            """.format(*urls))
+    return mark_safe("""<section class="hidden-xs"></section>""")
