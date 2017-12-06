dbde0d10fc20e3747c8cc352207fae19c613e70a
Satori <KomeijiSatori07@gmail.com>
2017-12-06 22:35:43 +0800

    add backend render emoticon bar

--- blogs/templatetags/blog_tags.py
+++ blogs/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
 from urllib.parse import urlencode, urlparse, parse_qs
+from django.utils.safestring import mark_safe
 from django import template
+from django.conf import settings
+import os
 
 
 register = template.Library()
@@ -22,3 +25,38 @@ def url_replace(context, **kwargs):
         query = context['request'].GET.dict()
         query.update(kwargs)
         return urlencode(query)
+
+
+@register.simple_tag
+def emoticon_bar():
+    cur_dir = os.path.join(settings.MEDIA_ROOT, "emoticon")
+    emoticon_set_names = [x for x in os.listdir(cur_dir) if os.path.isdir(os.path.join(cur_dir, x))]
+    emoticon_dirs = [os.path.join(cur_dir, x) for x in emoticon_set_names]
+    tab_list = ""
+    img_list = ""
+    for set_ind, emoticon_set_name in enumerate(emoticon_set_names):
+        emoticon_dir = os.path.join(cur_dir, emoticon_set_name)
+        files = [x for x in os.listdir(emoticon_dir) if os.path.isfile(os.path.join(emoticon_dir, x))]
+        urls = [settings.MEDIA_URL + "emoticon/{}/{}".format(emoticon_set_name, x) for x in files]
+
+        img_list = ""
+        for ind, url in enumerate(urls):
+            img_list += """<img src="{}" data-ind={}>""".format(url, ind)
+        cur_set = """
+        <div class="emoticon-img" data-emoticon_set_name="{emoticon_set_name}">
+            {img_str}
+        </div>
+        """.format(emoticon_set_name=emoticon_set_name, img_str=img_list)
+        tab_list += """
+        <li data-ind={set_ind} data-selected="0"><div>{set_name}</div></li>
+        """.format(set_ind=set_ind, set_name=emoticon_set_name)
+
+    string = """
+    <div class="emoticon-div">
+        <div class="emoticon-tab">
+            {tab_list}
+        </div>
+        {img_list}
+    </div>
+    """.format(tab_list=tab_list, img_list=cur_set)
+    return mark_safe(string)
--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # Satori's personal website
 This is the source code of my site. If you have any questions or suggestions please let me know.
 [View my site immediately!][1]
-
 Thanks for your feedback~
+
+
+## Prepare
+### Add the background files
+Move all your background in `media/background` folder, at least 6 pictures are required.
+### Add the emoticon files
+Move all your emoticon folders in `media/emoticon` folder. In each emoticon folder, put all your emoticons in that folder.
 
   [1]: http://chongliu.me
 
